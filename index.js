15f8ec01daa584bd869999578200a1043fc0abf4
Buzzcoder000 <linsonlivin@gmail.com>
2025-03-02 23:24:15 +0530

    3.6

--- index.js
+++ index.js
@@ -43,24 +43,16 @@ app.get('/info', (req, res) => {
     
         res.status(204).end();
     });
-    app.use(express.json()); // Middleware to parse JSON requests
+    app.use(express.json()); 
 
     app.post('/api/persons', (req, res) => {
         const { name, number } = req.body;
     
-        // Check if name or number is missing
-        if (!name || !number) {
-            return res.status(400).json({ error: 'Name or number is missing' });
-        }
-    
-        
-        const nameExists = persons.some(p => p.name === name);
-        if (nameExists) {
-            return res.status(400).json({ error: 'Name must be unique' });
-        }
+      
+
     
         const newPerson = {
-            id: Math.floor(Math.random() * 1000000).toString(), // Convert to string
+            id: Math.floor(Math.random() * 1000000).toString(), 
             name,
             number
         };

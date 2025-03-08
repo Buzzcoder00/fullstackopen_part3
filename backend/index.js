6e17236b2558e1fc053c40ff74005c643620da06
Buzzcoder000 <linsonlivin@gmail.com>
2025-03-08 13:25:04 +0530

    fixes

--- backend/index.js
+++ backend/index.js
@@ -10,19 +10,13 @@ morgan.token('post-data', (req) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post-data'));
 
 app.use(express.static('dist'));
-
 let persons = [
     { id: "1", name: "Arto Hellas", number: "040-123456" },
     { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
     { id: "3", name: "Dan Abramov", number: "12-43-234345" },
     { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" }
 ];
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve('build', 'index.html'));
-  });
-app.get("/", (req, res) => {
-    res.send("Server is running on Render!");
-  });
+
 app.get('/api/persons', (req, res) => {
     res.json(persons);
 });
